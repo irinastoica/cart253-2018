3d8182be1ffc135984b1ee50ce67bfe41be0d8f1
Irina <irinaioanastoica@gmail.com>
2018-09-20 11:18:53 -0400

    add the images and  the movement

--- exercises/exercise1/js/script.js
+++ exercises/exercise1/js/script.js
@@ -13,6 +13,24 @@ var clownImage;
 var clownImageX;
 var clownImageY;
 
+// The image of a dog image
+var dogImage;
+// The current position of the dog image
+var dogImageX;
+var dogImageY;
+
+// The image of a ball image
+var ballImage;
+// The current position of the ball image
+var ballImageX;
+var ballImageY;
+
+// The image of a ball image
+var candyImage;
+// The current position of the ball image
+var candyImageX;
+var candyImageY;
+
 // The transparent image of "felt" that wipes down the canvas
 var feltTextureImage;
 // The current position of the transparent image of "felt"
@@ -20,12 +38,16 @@ var feltTextureImageX;
 var feltTextureImageY;
 
 
+
 // preload()
 //
-// Load the two images we're using before the program starts
+// Load the three images we're using before the program starts
 
 function preload() {
   clownImage = loadImage("assets/images/clown.png");
+  dogImage = loadImage("assets/images/dog-image.png");
+  ballImage = loadImage("assets/images/ball.png");
+  candyImage = loadImage("assets/images/candy.png");
   feltTextureImage = loadImage("assets/images/black-felt-texture.png");
 }
 
@@ -41,6 +63,15 @@ function setup() {
   // Start the clown image at the centre of the canvas
   clownImageX = width/2;
   clownImageY = height/2;
+
+  // Start the dog image at the left side of the canvas and moves from left to right across the canvas
+  dogImageX = 0;
+  dogImageY = height/2;
+
+
+  // Start the candy image at the centre of the canvas
+  candyImageX = width/2;
+  candyImageY = height/2;
 
   // Start the felt image perfectly off screen above the canvas
   feltTextureImageX = width/2;
@@ -72,7 +103,30 @@ function draw() {
   // Add 1/10th of the x and y distance to the clown's current (x,y) location
   clownImageX = clownImageX + xDistance/10;
   clownImageY = clownImageY + yDistance/10;
-
   // Display the clown image
   image(clownImage,clownImageX,clownImageY);
+
+
+  // move the candy towards the right and slightly up
+    candyImageX += 1;
+    candyImageY -= 2;
+
+  // Display the candy image
+  image(candyImage,candyImageX,candyImageY);
+
+  // Start ball image exactly where the mouse is
+   ballImageX = mouseX;
+   ballImageY = mouseY;
+   ballImage.height = 200;
+   ballImage.width = 201;
+  // Display the clown image
+  image(ballImage,ballImageX,ballImageY);
+
+  // Move the dog image right by increasing its x position
+  dogImageX += 1;
+  // Display the dog image
+  image(dogImage,dogImageX,dogImageY);
+
+
+
 }
