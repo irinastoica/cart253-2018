cf99d1035d97073cb3fadf548b7e0e998ad7467d
Irina <irinaioanastoica@gmail.com>
2018-10-25 19:58:12 -0400

    change the paddle

--- exercises/exercise6/js/Paddle.js
+++ exercises/exercise6/js/Paddle.js
@@ -1,16 +1,12 @@
-//////////////// FIXED
-// Paddle constructor should be a comment, so preceded by "//"
-// Paddle constructor
+// Paddle
 //
 // A class that defines how a paddle behaves, including the ability
 // to specify the input keys to move it up and down
-
-//////////////// FIXED
-// The next line of comment should be preceded by "//"
-//Sets the properties with the provided arguments or defaults
-//
-//////////////// FIXED
-// Syntax error - changed "Pladdle" to "Paddle"
+////////FIXED
+// Paddle constructor
+// ////////FIXED
+// Sets the properties with the provided arguments or defaults
+//////FIXED
 function Paddle(x,y,w,h,speed,downKey,upKey) {
   this.x = x;
   this.y = y;
@@ -18,34 +14,26 @@ function Paddle(x,y,w,h,speed,downKey,upKey) {
   this.yv = 0;
   this.w = w;
   this.h = h;
-  //////////////// FIXED
-  // Syntax error - removed the extra e in "speeed" to "speed"
+  //////FIXED
   this.speed = speed;
   this.downKey = downKey;
-  this.upKey = upKey;
+  this.upKey = upKey;0.
 }
 
 // handleInput()
 //
 // Check if the up or down keys are pressed and update velocity
 // appropriately
-//
-//////////////// FIXED
-// Syntax error - changed "proto" to "prototype"
-// Logical error - changed "keyIsDown" instead of keyDown so the movement stops when the key is not pressed
-// Syntax error - changed "(upKey)" and "(downKey)" to "(this.upKey)" and ("this.downKey")
+//////FIXED
 Paddle.prototype.handleInput = function() {
+  //////FIXED
   if (keyIsDown(this.upKey)) {
     this.vy = -this.speed;
   }
-  //////////////// FIXED
-  // Behavioural error - Changed the sign of "this.speed" .The speed should be positive since the down key is pressed
   else if (keyIsDown(this.downKey)) {
-    this.vy = this.speed;
-  }
-  //////////////// FIXED
-  // Behavioural error - Added the else statement. The paddle should
-  // not move if no key is pressed, so if else the vy should be to 0
+   this.vy = this.speed;
+ }
+  //////FIXED
   else {
     this.vy = 0;
   }
@@ -56,19 +44,13 @@ Paddle.prototype.handleInput = function() {
 // Constrain the resulting position to be within the canvas
 Paddle.prototype.update = function() {
   this.y += this.vy;
-  //////////////// FIXED
-  // Syntax errors - changed "constraint" to "constrain" and "hight" to "height"
-  this.y = constrain(this.y,0,height-this.h);
+  this.y = constraint(this.y,0,hight-this.h);
 }
 
 // display()
 //
 // Draw the paddle as a rectangle on the screen
-//
-//////////////// FIXED
-// Syntax errors - changed "disploy" to "display" and removed an extra closing parenthese at "function()"
-Paddle.prototype.display = function() {
-  //////////////// FIXED
-  // Syntax error - changed "rectangle" to "rect"
-  rect(this.x,this.y,this.w,this.h);
+Paddle.prototype.disploy = function() {
+  //////FIXED
+  rectangle(this.x,this.y,this.w,this.h);
 }
