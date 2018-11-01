a0f62bf19b0bf29ed1fd556ae75ac7a38151b25c
Irina <irinaioanastoica@gmail.com>
2018-11-01 12:02:47 -0400

    Added the size of the ball

--- exercises/exercise6/js/Ball.js
+++ exercises/exercise6/js/Ball.js
@@ -58,7 +58,7 @@ Ball.prototype.isOffScreen = function () {
 // Draw the ball as a rectangle on the screen
 Ball.prototype.display = function () {
   // FIXED
-  rect(this.x,this.y);
+  rect(this.x,this.y,this.size, this.size);
 }
 
 // handleCollision(paddle)
