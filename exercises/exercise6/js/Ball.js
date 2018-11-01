226faec791945949836d426a65bc708496c4c963
Irina <irinaioanastoica@gmail.com>
2018-11-01 12:04:36 -0400

    Add the open paranthese of if

--- exercises/exercise6/js/Ball.js
+++ exercises/exercise6/js/Ball.js
@@ -69,7 +69,8 @@ Ball.prototype.display = function () {
 // FIXED
 Ball.prototype.handleCollision = function(paddle) {
   // Check if the ball overlaps the paddle on x axis
-  if (this.x + this.size > paddle.x && this.x < paddle.x + paddle.w) {
+  // FIXED
+  if ((this.x + this.size) > paddle.x && this.x < paddle.x + paddle.w))  {
     // Check if the ball overlaps the paddle on y axis
     if (this.y + this.size > paddle.y && this.y < paddle.y + paddle.h) {
       // If so, move ball back to previous position (by subtracting current velocity)
