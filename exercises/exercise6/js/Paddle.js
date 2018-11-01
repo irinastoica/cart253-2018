b90e86df4ba3098f38a73dcf57239ad7aba76b5c
Irina <irinaioanastoica@gmail.com>
2018-11-01 11:36:50 -0400

    Changed keyDown for keyIsDown

--- exercises/exercise6/js/Paddle.js
+++ exercises/exercise6/js/Paddle.js
@@ -32,7 +32,8 @@ Paddle.prototype.handleInput = function() {
   if (keyIsDown(upKey)) {
     this.vy = -this.speed;
   }
-  else if (keyDown(downKey)) {
+  // FIXED
+  else if (keyIsDown(downKey)) {
     this.vy = -this.speed;
   }
 }
