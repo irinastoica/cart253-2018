ad597c02f8dadf3da0faf98fed78ebb24cbd7d86
Irina <irinaioanastoica@gmail.com>
2018-11-08 11:15:15 -0500

    background-fixed

--- projects/project2/js/script.js
+++ projects/project2/js/script.js
@@ -19,24 +19,20 @@ var backgroundImage;
 // loading the images
 function preload() {
   backgroundImage = loadImage("assets/images/background.jpg");
-  dinoLeft = loadImage("assets/images/dinoleft.png");
 
 }
 // Creates the ball and paddles
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+createCanvas(1000, 597);
   // Create a ball
   ball = new Ball(width/2,height/2,5,5,10,5);
   // Create the right paddle with UP and DOWN as controls
-  rightPaddle = new Paddle(width-10,height/2,10,60,10,DOWN_ARROW,UP_ARROW);
+  rightPaddle = new Paddle(width - 10, height / 2, 10, 60, 10, DOWN_ARROW, UP_ARROW, 0);
   // Create the left paddle with W and S as controls
   // Keycodes 83 and 87 are W and S respectively
-  leftPaddle = new Paddle(0,height/2,10,60,10,83,87);
+   leftPaddle = new Paddle(0, height / 2, 10, 60, 10, 83, 87, 0);
 }
-function windowResized() {
-  // take the full size of the window
-  resizeCanvas(window.innerWidth, window.innerHeight);
-}
+
 
 // draw()
 //
